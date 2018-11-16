f5d4f9226503e2e4bf6dd28ca1c6f783ac7a782e
kungfupan <kungfupan@163.com>
2018-11-16 10:19:30 +0800

    werer

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('qwwe');
+console.log('再次修改');
+
+function test(){
+    if(1){
+        alert(1);
+    }
+    else{
+        alert(2);
+    }
+}
+
+function test1(){
+    //rtrtytryt
+    alert(1);
+}
+
+function test2(){
+    alert(2);
+}
+
+function test3(){
+    alert(3);
+}
+
+function test4(){
+    //qweweq123456
+    alert(4);
+}
